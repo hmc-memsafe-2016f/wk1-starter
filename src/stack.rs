724470422815facebf347476d9bcf4695c3616b4
Dan Obermiller <dannnno16@gmail.com>
2016-10-26 21:58:05 -0700

    All but remove_front and reverse

--- src/stack.rs
+++ src/stack.rs
@@ -3,12 +3,62 @@
 //
 // The implementation of SinglyLinkedList
 
+use std::mem;
 use Stack;
 
+struct Node<T> {
+    pointer: Option<Box<Node<T>>>,
+    data: T,
+}
+
 pub struct SinglyLinkedList<T> {
-    data: T // Probably not what you want
+    head: Option<Box<Node<T>>>,
+    size: usize,
 }
 
 impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
-    // ????
+    fn new() -> Self {
+        SinglyLinkedList{head: None, size: 0}
+    }
+
+    fn push_front(&mut self, item: T) {
+        let head = Box::new(Node {
+            pointer: self.head.take(),
+            data: item,
+        });
+        self.head = Some(head);
+        self.size += 1;
+    }
+
+    fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|n| {
+            let node = *n;
+            self.head = node.pointer;
+            self.size -= 1;
+            node.data
+        })
+    }
+
+    fn peek_front(&self) -> Option<&T> {
+        match self.head {
+            Some(ref node) => Some(&node.data),
+            None => None
+        }
+    }
+
+    fn len(&self) -> usize { self.size }
+
+    fn remove_first(&mut self, item: &T) -> Option<T> { None }
+
+    fn reverse(&mut self) {
+        let mut list = SinglyLinkedList::<T>::new();
+        loop {
+            match self.head {
+                Some(_) => list.push_front(self.pop_front().unwrap()),
+                None => break
+            }
+        }
+
+        mem::swap(&mut self.head, &mut list.head);
+    }
 }
