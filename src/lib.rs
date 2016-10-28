9d777b68e90249920246507f2a4b5a1c4a14c5ce
Michael Sheely <msheely@hmc.edu>
2016-10-28 14:57:49 -0700

    Finished implementing required functions

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@ pub trait Stack<T> {
     fn pop_front(&mut self) -> Option<T>;
     fn peek_front(&self) -> Option<&T>;
     fn len(&self) -> usize;
-    fn remove_first(&mut self, _: &T) -> Option<T> { None }
+    /*
     fn reverse(&mut self) { }
+    fn remove_first(&mut self, _: &T) -> Option<T> { None }
+    */
 }
--- src/stack.rs
+++ src/stack.rs
@@ -1,14 +1,61 @@
-// Alex Ozdemir <aozdemir@hmc.edu> // <- Your name should replace this line!
-// Starter code for HMC's MemorySafe, week 1
+// Michael Sheely <msheely@hmc.edu>
+// Developed from the Starter code for HMC's MemorySafe, week 1
 //
 // The implementation of SinglyLinkedList
 
 use Stack;
 
 pub struct SinglyLinkedList<T> {
-    data: T // Probably not what you want
+    first: Option<Box<Node<T>>>,
+    size: usize,
+}
+
+struct Node<T> {
+    payload: T,
+    next: Option<Box<Node<T>>>,
 }
 
 impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
+    fn new() -> Self {
+        SinglyLinkedList{first: None, size: 0}
+    }
+
+    fn push_front(&mut self, item: T) {
+        self.first = Some(Box::new(Node{payload: item, next: self.first.take()}));
+        self.size += 1;
+    }
+
+    fn pop_front(&mut self) -> Option<T> {
+        if self.size > 0 {self.size -= 1;}
+        self.first.take().map(|mut node| {self.first = node.next.take(); node.payload})
+    }
+    
+    fn peek_front(&self) -> Option<&T> {
+        match self.first {
+            Some(ref node) => Some(&node.payload),
+            None => None,
+        }
+    }
+
+    fn len(&self) -> usize { self.size }
+
+    //fn remove_first(&mut self, _: &T) -> Option<T> {}
+    /*
+    fn reverse(&mut self) {
+        let mut node : Node<T>;
+        match self.first {
+            Some(x) => {node = *x;},
+            None => {return;},
+        }
+        loop {
+            match node.next {
+                Some(next_node) => next_node.next.take(),
+                None => self.first,
+            };
+            // let mut next = node.next.take();
+            // node = //the next one
+        }
+    }
+    */
     // ????
 }
