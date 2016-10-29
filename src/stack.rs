17e8c2c2199e2312f68d7812e218aa25b40b91bb
Daniel <daniel.sonner@gmail.com>
2016-10-29 14:53:33 -0700

    think I got pop_front via mem replace

--- src/stack.rs
+++ src/stack.rs
@@ -4,6 +4,7 @@
 // The implementation of SinglyLinkedList
 
 use Stack;
+use std::mem;
 
 pub struct Node<T> {
     data: T,
@@ -21,24 +22,18 @@ impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
     }
     fn push_front(&mut self, item: T)
     {
-        let nextNode = match self.first.take() {
+        let next_node = match self.first.take() {
             None => None,
             Some(d) => Some(d)
         };
-        self.first = Some(Box::<Node<T>>::new(Node{data: item, next: nextNode}));
+        self.first = Some(Box::<Node<T>>::new(Node{data: item, next: next_node}));
     }
     fn pop_front(&mut self) -> Option<T>
     {
-        let mut retOption = self.first.take();
-        let mut retVal : Option<T> = match retOption {
+        match self.first.take() {
             None => None,
-            Some(d) => Some(d.data)
-        };
-        self.first = match retOption {
-            None => None,
-            Some(d) => d.next.take()
-        };
-        retVal
+            Some(mut d) => {self.first = mem::replace(&mut d.next, None); Some(d.data)}
+        }
     }
     fn peek_front(&self) -> Option<&T> {unimplemented!()}
     fn len(&self) -> usize {unimplemented!()}
