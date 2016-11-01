63da3e5bc674c8a6b77c19c6563c443c2ebaadb5
rmawhorter <rmawhorter@g.hmc.edu>
2016-10-31 22:23:12 -0700

    fixed some comments, passes non-bonus tests

--- src/stack.rs
+++ src/stack.rs
@@ -28,29 +28,22 @@ impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
 
     fn push_front(&mut self, item: T)
     {
+    	//create a new node with the data
     	let mut new_first = Node{data: item, next: None};
 
-    	//TWO things need to happen here
-    	//1. the new node gets first as its next
-    	//2. the first gets the boxed new node
-
-    	//need to take ownership of the first node in order to give it to the second node
-    	//the list needs to take ownership of the new node.
-
+    	//new first node takes ownership of the old first node
     	mem::swap(&mut new_first.next, &mut self.first);
     	//now new_first has self.first and self.first has None
 
+    	//give the list ownership of the new first node
     	self.first = Some(Box::new(new_first));
     	self.length += 1;
     }
 
     fn pop_front(&mut self) -> Option<T>
     {
-		//create an empty option node
-		let empty_node = None;
-
-		//replace the first element and take ownership of it
-		let old_node = mem::replace(&mut self.first, empty_node);
+		//replace the first element to take ownership of it
+		let old_node = mem::replace(&mut self.first, None);
 
 		match old_node
 		{
@@ -60,13 +53,14 @@ impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
 			//and return its data.
 			Some(box_node) => {
 								let node = *box_node;
-								mem::replace(&mut self.first, node.next);
+								self.first = node.next;
 								self.length -= 1;
 								Some(node.data)
 							 },
 			//If we didn't have a node to pop, just return none
 			//and put none back on first.
 			None => {
+						//not strictly necessary...
 						mem::replace(&mut self.first, None);
 						None
 					},
@@ -75,7 +69,6 @@ impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
 
     fn peek_front(& self) -> Option<&T>
     {
-    	//TODO: no match
     	self.first.as_ref().map(|node| &node.data)
     }
 
@@ -87,12 +80,12 @@ impl<T: Eq> Stack<T> for SinglyLinkedList<T> {
 	/*
     fn remove_first(&mut self, _: &T) -> Option<T> { None }
     {
-    	None
+    	
     }
 
     fn reverse(&mut self)
     {
-
+		//needs to take ownership of each node in turn and push it back onto the stack
     }
     */
 }
